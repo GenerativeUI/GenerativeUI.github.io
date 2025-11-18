f1eee8a1923b843ce697df7355463eccf9019b3e
Matan Kalman <matank@google.com>
2025-11-18 07:21:12 -0800

    Initial project upload

--- static/js/index.js
+++ static/js/index.js
@@ -5,17 +5,37 @@ $(document).ready(function() {
     // Check for click events on the navbar burger icon
 
     var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
+		slidesToScroll: 1,
+		slidesToShow: 2,
+		loop: true,
+		infinite: true,
+		autoplay: false,
+		autoplaySpeed: 5000,
     }
 
-		// Initialize all div with carousel class
+    var options2 = {
+		slidesToScroll: 2,
+		slidesToShow: 2,
+		loop: true,
+		infinite: false,
+		autoplay: false,
+		autoplaySpeed: 5000,
+    }
+
+
+	// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
-	
+	var carousels2 = bulmaCarousel.attach('.carousel2', options2);
+
     bulmaSlider.attach();
 
+	function selectCategory(category) {
+		$('.carousel').hide();
+		$('#' + category + '-carousel').show();
+	}
+
+    $('#category-select').on('change', function() {
+		selectCategory($(this).val());
+    });
+	selectCategory('main');
 })
